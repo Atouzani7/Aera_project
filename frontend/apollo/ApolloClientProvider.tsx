4b92e51ae2608d487b094313f1220cb68994ad30
Asma <asma.touzani7@gmail.com>
2026-05-13 20:13:08 +0200

    fix docker , to local and mobile

--- frontend/apollo/ApolloClientProvider.tsx
+++ frontend/apollo/ApolloClientProvider.tsx
@@ -10,6 +10,12 @@ import { setContext } from "@apollo/client/link/context";
 import { useMemo } from "react";
 
 export default function ApolloClientProvider({ children }: { children: React.ReactNode }) {
+
+    const API_URL =
+        typeof window !== "undefined"
+            ? `http://${window.location.hostname}:4001/graphql`
+            : "http://backend:4001/graphql";
+
     const client = useMemo(() => {
         const httpLink = createHttpLink({
             uri: process.env.NEXT_PUBLIC_API_URL || "http://localhost:4001/graphql",
@@ -20,6 +26,7 @@ export default function ApolloClientProvider({ children }: { children: React.Rea
                 typeof window !== "undefined"
                     ? localStorage.getItem("token")
                     : null;
+
 
             return {
                 headers: {
